dc1ade2e079cc31820d6d981df52659d1c3f8886
Chicster <daniel.m.pryor@gmail.com>
2018-06-18 14:50:22 -0700

    Terminal style

--- search.js
+++ search.js
@@ -1,120 +1,163 @@
+// source script by /u/Will_Eccles
+// released under Apache-2.0 license
+// significant changes have been made from source
 
-var search = document.getElementById("search");
-var help = document.getElementById("search-help");
-var icon = document.getElementById("search-icon");
-var form = document.getElementById("search-form");
+var box = document.getElementById("search");
 
-var commands = [
-	{
-		command: "",
-		label: "Google",
-		icon: "fa fa-google",
-		url: "https://www.google.com/search?q="
-	},
-	{
-		command: "/a",
-		label: "Amazon",
-		icon: "fa fa-amazon",
-		url: "https://www.amazon.com/s/field-keywords="
-	},
-	{
-		command: "/d",
-		label: "DuckDuckGo",
-		icon: "fa fa-ban",
-		url: "https://duckduckgo.com/?q="
-	},
-	{
-		command: "/f",
-		label: "Facebook",
-		icon: "fa fa-facebook",
-		url: "https://www.facebook.com/search/top/?q="
-	},
-	{
-		command: "/t",
-		label: "Twitter",
-		icon: "fa fa-twitter",
-		url: "https://twitter.com/search?q="
-	},
-	{
-		command: "/r",
-		label: "Reddit",
-		icon: "fa fa-reddit-alien",
-		url: "https://www.reddit.com/search?sort=relevance&t=all&q="
-	},
-	{
-		command: "/w",
-		label: "Wikipedia",
-		icon: "fa fa-wikipedia-w",
-		url: "http://en.wikipedia.org/wiki/Special:Search/"
-	},
-	{
-		command: "/y",
-		label: "YouTube",
-		icon: "fa fa-youtube-play",
-		url: "https://www.youtube.com/results?search_query="
-	},
-];
+// catch URLs
+var urlPattern = /^(https?:\/\/)?[^ ]+[.][^ ]+([.][^ ]+)*(\/[^ ]+)?$/i;
 
-var command = commands[0];
+// convenient links
+var handy = /^(google|gmail|dropbox|github)$/i;
 
-search.addEventListener("keyup", function(e) {
-	var value = search.value;
-
-	if (value.indexOf("/?") == 0) {
-		help.style.opacity = 1;
-		help.style["max-height"] = "1000px";
-	} else if (e.keyCode == 13 || e.which == 13) {
-		if (value.indexOf(".") > 0) {
-			window.location.href = "http://" + encodeURIComponent(value);
-		} else {
-			window.location.href = command.url + encodeURIComponent(value);
-		}
-	} else if (value[0] == "/" && value[value.length - 1] == " ") {
-		value = value.trim();
-		for (var i = 0; i < commands.length; i++) {
-			if (value == commands[i].command) {
-				command = commands[i];
-				icon.className = command.icon;
-				search.value = "";
-				form.setAttribute("action", command.url);
-				break;
-			}
-		}
-	}
-});
-
-search.addEventListener("keydown", function(e) {
-	var value = search.value;
-	var key = e.keyCode || e.which;
-
-	if (key == 0 || key == 229) {
-		key = isBackspace(value) ? 8 : 0;
-	}
-
-	if (key == 8) {
-		if (value == "" && command.icon != commands[0].icon) {
-			command = commands[0];
-			icon.className = command.icon;
-			search.value = "";
-			form.setAttribute("action", command.url);
-		}
-		help.style.opacity = 0;
-		help.style["max-height"] = "100px";
-	}
-});
-
-/* Fix for Android keycode 229 issue */
-var prevWord = "";
-function isBackspace(val) {
-	var bool = val && val.length < prevWord.length;
-	prevWord = val;
-	return bool;
+// search for text in text box
+function search() {
+  console.log("Googling \"" + box.value + "\"");
+  console.log("Encoded query: \n" + encodeURIComponent(box.value));
+  document.location.href = "https://searx.me/?q=" + encodeURIComponent(box.value);
 }
 
-help.innerHTML = "";
-for (var i = 0; i < commands.length; i++) {
-	var command = commands[i];
-	if (command.command.length > 0) {
-		help.innerHTML += "<li><span><span class='icon " + command.icon + "'></span><span class='command'>" + command.command + "</span></span></li>";
-	}
+// if not search text, navigate to URL
+function nav(address) {
+  // if the address starts with "https?|ftp ://"
+  if (/^(?:(?:https?|ftp):\/\/).*/i.test(address)) {
+    document.location.href = address;
+  } else {
+    document.location.href = "http://" + address;
+  }
+}
+
+// handle enter key press in text box
+// if text is a command, handle command parsing
+function searchKeyPress(e) {
+  e = e || window.event;
+  if (e.keyCode == 13) {
+    parseCom(box.value);
+  }
+
+  // first, handle known cases of preset commands
+}
+
+// parse command
+function parseCom(com) {
+  // handle convenient links
+  if (handy.test(com)) {
+    nav("http://www."+com+".com/");
+  }
+  // handle "drive" command
+  else if (/^drive$/i.test(com)) {
+    nav("http://drive.google.com");
+  }
+  // handle "reddit" command
+  else if (com.startsWith("reddit")==true) {
+    // if "reddit" command is matched, open reddit
+    if (/^reddit$/i.test(com)) {
+      nav("https://www.reddit.com/");
+    }
+    // if the subreddit command is matched
+    else if (/^reddit -r .*$/i.test(com)) {
+      var sargs = com.split(" ");
+      nav("https://www.reddit.com/r/" + sargs.pop());
+    }
+    // if the user command is matched
+    else if (/^reddit -u .*$/i.test(com)) {
+      var uargs = com.split(" ");
+      nav("https://www.reddit.com/u/" + uargs.pop());
+    }
+    // if any of the custom subreddit commands are matched
+    else if (/^reddit [A-Za-z]{2,2}$/i.test(com)) {
+      var subargs = com.split(" ");
+      switch (subargs.pop()) {
+        case "bs":
+          nav("https://www.reddit.com/r/battlestations/");
+          break;
+        case "pc":
+          nav("https://www.reddit.com/r/pcmasterrace/");
+          break;
+        case "up":
+          nav("https://www.reddit.com/r/unixporn/");
+          break;
+        case "sp":
+          nav("https://www.reddit.com/r/startpages/");
+          break;
+        default:
+          nav("https://www.reddit.com/");
+          break;
+        }
+      }
+      // if anything else, search for it
+      else if (urlPattern.test(com)){
+        nav(com);
+      }
+      // if all else fails, search for it
+      else {
+        search();
+      }
+    }
+    // handle "twt"" command
+    else if (com.startsWith("twt")==true) {
+      // if "twt" command is matched, open twitter
+      if (/^twt$/i.test(com)) {
+        nav("https://www.twitter.com/");
+      }
+      // if the user command is matched
+      else if (/^twt -u .@?[A-Za-z0-9_]{1,15}$/i.test(com)) {
+        var targs = com.split(" ");
+        nav("https://www.twitter.com/" + targs.pop());
+      }
+      // if search command is matched
+      else if (/^twt -s .{1,140}$/i.test(com)) {
+        var query = com.replace(/^twt -s /i, "");
+        nav("https://www.twitter.com/search?q=" + encodeURIComponent(query));
+      }
+      // if anything else, search for it
+      else if (urlPattern.test(com)){
+        nav(com);
+      }
+      // if all else fails, search for it
+      else {
+        search();
+      }
+    }
+  // handle "ig" command
+  else if (com.startsWith("ig")==true) {
+    // if "ig" command is matched, open instagram
+    if (/^ig$/i.test(com)) {
+      nav("https://www.instagram.com/");
+    }
+    // if the user command is matched
+    else if (/^ig -u .@?[A-Za-z0-9_.]{1,30}/i.test(com)) {
+      var iargs = com.split(" ");
+      nav("https://www.instagram.com/" + iargs.pop());
+    }
+    // if anything else, search for it
+    else if (urlPattern.test(com)){
+      nav(com);
+    }
+    // if all else fails, search for it
+    else {
+      search();
+    }
+  }
+  // handle "youtube" and "yt" commands
+  else if (/^youtube$/i.test(com) || /^yt$/i.test(com)) {
+    nav("https://www.youtube.com/feed/subscriptions");
+  }
+  // handle "twitch" and "ttv" commands
+  else if (/^(twitch|ttv)$/i.test(com)) {
+    nav("http://www.twitch.tv/following");
+  }
+  else if (/^(twitch|ttv) -u .[^ ]+$/i.test(com)) {
+    var parts = com.split(" ");
+    nav("http://www.twitch.tv/" + parts.pop());
+  }
+  // if text doesn't match any of the commands...
+  // ... but is a valid URL
+  else if (urlPattern.test(com)) {
+    nav(com);
+  }
+  // ... or should be searched
+  else {
+    search();
+  }
 }
